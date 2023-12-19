10790bd8950975346207f90bd32f0159a3e78bde
Omkarpisal-10 <omkarpisal2024@gmail.com>
2023-12-19 11:31:19 +0530

    Solved

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -13,8 +13,60 @@
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
+
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  // Create an object to store total spending for each category
+  const categoryTotalMap = {};
+
+  // Iterate through each transaction
+  transactions.forEach((transaction) => {
+    const { category, price } = transaction;
+
+    // If the category is not in the map, add it with the current transaction's price
+    if (!categoryTotalMap[category]) {
+      categoryTotalMap[category] = price;
+    } else {
+      // If the category is already in the map, add the current transaction's price to the total
+      categoryTotalMap[category] += price;
+    }
+  });
+
+  // Convert the object into an array of objects with category and totalSpent properties
+  const result = Object.keys(categoryTotalMap).map((category) => ({
+    category,
+    totalSpent: categoryTotalMap[category],
+  }));
+
+  return result;
 }
+
+// Example usage:
+const transactions = [
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  },
+  {
+    id: 2,
+    timestamp: 1656080400000,
+    price: 20,
+    category: 'Clothing',
+    itemName: 'T-Shirt',
+  },
+  {
+    id: 3,
+    timestamp: 1656084000000,
+    price: 15,
+    category: 'Food',
+    itemName: 'Burger',
+  },
+];
+
+const result = calculateTotalSpentByCategory(transactions);
+console.log(result);
+
 
 module.exports = calculateTotalSpentByCategory;
